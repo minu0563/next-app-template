b93f05dac80bf5c1dde25a476927691471db7e81
MINU <dgwag4977@gmail.com>
2025-12-16 17:17:50 +0900

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello Next.js</h1>
+      <p>이 페이지가 보이면 정상입니다.</p>
+      <a href="/app2">여기를 눌러 새 페이지로 이동해 보세요.</a>
+    </div>
+  );
+}
